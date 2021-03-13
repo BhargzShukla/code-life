92c6cbfb66d49d64284670c6323e01adf9b69d87
Bhargav Shukla <shukla.bhargav.k@gmail.com>
2021-03-14 01:51:20 +0530

    Adjustments

--- server.js
+++ server.js
@@ -8,15 +8,11 @@ const io = require("socket.io")({
 let currentCode = "";
 
 io.on("connection", (socket) => {
-  console.log("connected to socket.io");
-  // socket.join("codeRoom");
   socket.on("clientSend", (arg) => {
     currentCode = arg;
-    console.log("currentCode = ", currentCode);
     socket.broadcast.emit("serverSend", currentCode);
   });
 });
 
-const port = 8000;
+let port = process.env.PORT ?? 8000;
 io.listen(port);
-console.log("listening on port ", port);
