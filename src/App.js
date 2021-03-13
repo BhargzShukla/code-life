d677df6a0df47902af4bcb4ce52d6fbf5b3d29b5
Bhargav Shukla <shukla.bhargav.k@gmail.com>
2021-03-14 02:02:00 +0530

    More ajustments

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import openSocket from "socket.io-client";
 import "./App.css";
 
-const socket = openSocket("http://localhost:8000");
+const socket = openSocket(
+  `https://warm-brushlands-48090.herokuapp.com:${process.env.PORT ?? 8000}`
+);
 function App() {
   const [clientCode, setClientCode] = useState("");
 
